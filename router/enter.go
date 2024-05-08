68029bcaaaacc7ebe54b5d9992123e170b640c32
hws <395621926@qq.com>
2024-05-08 10:03:37 +0800

    两种不同的思路，一种是go的鸭子类型，还有一种是java的分层思路

--- router/enter.go
+++ router/enter.go
@@ -11,13 +11,19 @@ type ApiRouter struct {
 
 var apiRouterGroup = new(ApiRouter)
 
+type RouterGroup struct {
+	*gin.Engine
+}
+
 func InitRouter() *gin.Engine {
 	gin.SetMode(global.CONFIG.System.Env)
 	router := gin.Default()
 
 	settingsApi := api.ApiGroupApp.SettingsApi
 	// 系统设置
-	router.GET("", settingsApi.InfoView)
+	router.GET("infoview", settingsApi.InfoView)
 
+	routerGroupApp := RouterGroup{router}
+	routerGroupApp.SettingsRouter()
 	return router
 }
--- router/settings_router.go
+++ router/settings_router.go
@@ -8,6 +8,18 @@ import (
 func (ApiRouter) InitSettings(Router *gin.RouterGroup) {
 	routerApi := api.ApiGroupApp.SettingsApi
 	{
-		Router.GET("site_info", routerApi.InfoView)
+		Router.GET("site_info", routerApi.SettingsInfoView)
+	}
+}
+
+func (router RouterGroup) SettingsRouter() {
+	settingsApi := api.ApiGroupApp.SettingsApi
+	router.GET("", settingsApi.SettingsInfoView)
+}
+
+func (ApiRouter) SettingsRouter1(Router *gin.RouterGroup) {
+	routerApi := api.ApiGroupApp.SettingsApi
+	{
+		Router.GET("/site", routerApi.InfoView)
 	}
 }
