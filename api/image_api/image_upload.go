3683a4f8640292350480a5f143bd51d39ee9bb39
hws <395621926@qq.com>
2024-05-10 23:49:33 +0800

    判断文件大小限制和路径限制功能的增加

--- api/image_api/image_upload.go
+++ api/image_api/image_upload.go
@@ -8,6 +8,12 @@ import (
 	"path"
 )
 
+type FileUploadResponse struct {
+	FileName  string `json:"file_name"`  // 文件名
+	IsSuccess bool   `json:"is_success"` // 是否成功
+	Msg       string `json:"msg"`        // 消息
+}
+
 // ImageUploadView 上传图片
 func (i ImageApi) ImageUploadView(c *gin.Context) {
 
@@ -23,9 +29,19 @@ func (i ImageApi) ImageUploadView(c *gin.Context) {
 		response.FailWithMessage("不存在文件，请上传图片--", c)
 		return
 	}
-
+	var resList []FileUploadResponse
 	for _, file := range files {
 		filePath := path.Join("uploads", file.Filename)
+		//判断大小
+		size := float64(file.Size) / float64(1024*1024)
+		if size > 2 {
+			resList = append(resList, FileUploadResponse{
+				FileName:  file.Filename,
+				IsSuccess: false,
+				Msg:       "文件大小超过2M",
+			})
+			continue
+		}
 		err := c.SaveUploadedFile(file, filePath)
 		if err != nil {
 			global.LOG.Error(err)
